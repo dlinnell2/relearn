2b906f458d898a174aca1f612b191c3d20e5d575
Donald Linnell <donald.linnell@gmail.com>
2020-08-14 21:01:43 -0500

    Adds bootstrap

---
+++ src/components/HelloBootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HelloBootstrap = () => 
+    <div className="container">
+
+        <div className="navbar">
+            text inside navbar
+        </div>
+        <div className="jumbotron">Hello World</div>
+        <div className="card">
+            <div className="card-body">
+                Inside Card
+            </div>
+        </div>
+
+
+    </div>
+
+
+export default HelloBootstrap;
