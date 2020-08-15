4a6b7047cc6d71a0d412e66a7e2ae5e504833b15
Donald Linnell <donald.linnell@gmail.com>
2020-08-14 20:48:09 -0500

    Second activity - rendering items

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HelloDiv = () => 
+<div>
+    <h1>Hello my name is Donald!</h1>
+    <ul>
+        <li>Books</li>
+        <li>Records</li>
+        <li>Cats</li>
+        </ul>;
+
+    </div>
+
+export default HelloDiv;
