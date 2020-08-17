fea7a2b4c51ec14a274cf57b0863fc43f1abb762
Donald Linnell <donald.linnell@gmail.com>
2020-08-16 20:54:33 -0500

    Modifying state more fully, passing functions in props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React from "react";
-import Counter from "./components/Counter";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-const App = () => <Counter />;
+class App extends React.Component {
+
+  state = {
+    friends
+  }
+
+  removeFriend = id =>{
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    this.setState({friends})
+  }
+
+  render() {
+    return (
+  <Wrapper>
+    <h1 className="title">Friends List</h1>
+    {this.state.friends.map(friend =>
+      <FriendCard
+        id = {friend.id}
+        name={friend.name}
+        image={friend.image}
+        occupation={friend.occupation}
+        location = {friend.location}
+        removeFriend = {this.removeFriend}
+        />
+    )}
+  </Wrapper>
+    )}
+
+};
 
 export default App;
